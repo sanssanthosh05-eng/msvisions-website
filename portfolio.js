9fe7b7a84b5555efada0597468e46f5e281749a0
sanssanthosh05-eng <sanssanthosh05@gmail.com>
2026-07-16 18:49:59 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,33 @@
+const buttons = document.querySelectorAll(".portfolio-filter button");
+const cards = document.querySelectorAll(".portfolio-card");
 
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const filter = button.dataset.filter;
+
+        cards.forEach(card => {
+
+            if (filter === "all") {
+
+                card.style.display = "block";
+
+            } else if (card.classList.contains(filter)) {
+
+                card.style.display = "block";
+
+            } else {
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+});
